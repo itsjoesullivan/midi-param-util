e1f5c05811fe11509b680613556b8ad77ee96ece
Joe Sullivan <itsjoesullivan@gmail.com>
2016-06-30 17:36:32 -0400

    Init

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  getTimeValueFromMIDIParam: function getTimeValueFromMIDIParameter(midiValue) {
+    return 128 * Math.pow(4, Math.log2(value / 16)) / 1000;
+  },
+  getFrequencyValueFromMIDIParam: function getTimeValueFromMIDIParameter(midiValue) {
+    return 8 + (44100 / 2) * Math.pow(midiValue / 127, 4);
+  }
+};
